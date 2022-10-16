c536dad2e973eefb649a3f7ee3d6a27f88af41da
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-10-16 19:12:24 +0200

    Started php

---
+++ php/signup.php
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+<h1>Sign-Up</h1>
+<form method="POST">
+    username:<br>
+    <input type="text" name="username">
+    <br>
+    password:<br>
+    <input type="password" name="password">
+    <br>
+    <input type="submit" value="sign-up">
+</form>
